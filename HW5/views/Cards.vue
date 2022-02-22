1fda7a5b1278362e4f74da1d288783f471dd3af9
Gadzhi <91538968+Gadzhizhka@users.noreply.github.com>
2022-02-22 12:57:51 +0300

    Последнее задание

---
+++ HW5/views/Cards.vue
@@ -0,0 +1,18 @@
+<template>
+  <mini-card v-for="good in goods" :key="good.id" :object="good"/>
+</template>
+
+<script>
+import MiniCard from "../components/MiniCard";
+export default {
+  components: {MiniCard},
+  computed: {
+    goods() {
+      return this.$store.state.goods;
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
